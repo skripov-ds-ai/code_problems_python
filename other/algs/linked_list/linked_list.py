83f18f7b9b39c5830ddf017d8f7e2f84bf258454
Denis Skripov <nizhikebinesi@gmail.com>
2020-10-02 23:34:16 +0600

    add first task to other

---
+++ other/algs/linked_list/linked_list.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        # accumulator
+        self._length = 0
+
+    def add_in_tail(self, item):
+        if self.head is None:
+            self.head = item
+        else:
+            self.tail.next = item
+        self.tail = item
+        self._length += 1
+
+    def print_all_nodes(self):
+        node = self.head
+        while node != None:
+            print(node.value)
+            node = node.next
+
+    def find(self, val):
+        node = self.head
+        while node is not None:
+            if node.value == val:
+                return node
+            node = node.next
+        return None
+
+    def find_all(self, val):
+        elems = []
+        node = self.head
+        while node:
+            if node.value == val:
+                elems.append(node)
+            node = node.next
+        return elems
+        # здесь будет ваш код
+
+    def delete(self, val, all=False):
+        node = self.head
+        prev = None
+
+        if node and node.value == val:
+            while node and node.value == val:
+                self._length -= 1
+                node = node.next
+                if not all:
+                    return
+            prev = node
+            self.head = node
+
+        while node:
+            if node.value == val:
+                while node and node.value == val:
+                    node = node.next
+                    self._length -= 1
+                    if not all:
+                        return
+                prev.next = node
+            prev = node
+            if node:
+                node = node.next
+        # здесь будет ваш код
+
+    def clean(self):
+        self.head = None
+        self.tail = None
+        self._length = 0
+        # здесь будет ваш код
+
+    def len(self):
+        return self._length
+        # здесь будет ваш код
+
+    def insert(self, afterNode, newNode):
+        self._length += 1
+
+        if not afterNode:
+            newNode.next = self.head
+            self.head = newNode
+            return
+
+        node = self.head
+        while node is not afterNode:
+            node = node.next
+        if node.next and node.next.next:
+            newNode.next = node.next.next
+        node.next = newNode
+        # здесь будет ваш код
+
---
+++ other/algs/linked_list/test_linkedList.py
@@ -0,0 +1,76 @@
+from unittest import TestCase
+from other.algs.linked_list.linked_list import LinkedList, Node
+
+
+class TestLinkedList(TestCase):
+    def make_list(self, *vals):
+        l = LinkedList()
+        nodes = []
+        for v in vals:
+            node = Node(v)
+            l.add_in_tail(node)
+            nodes.append(node)
+        return l, nodes
+
+    def test_find_all(self):
+        selected = 1
+        l, nodes = self.make_list(1, 1, 1, 42, 1)
+        nodes = [node for node in nodes if node.value == selected]
+        found = l.find_all(selected)
+        self.assertListEqual(nodes, found)
+
+    def test_delete_first(self):
+        selected = 1
+        l, _ = self.make_list(1, 1, 1, 42, 1)
+        size = l.len() - 1
+        l.delete(selected, all=False)
+        self.assertEqual(size, l.len())
+
+    def test_delete_first_empty(self):
+        selected = 1
+        l, _ = self.make_list()
+        size = 0
+        l.delete(selected, all=False)
+        self.assertEqual(size, l.len())
+
+    def test_delete_first_one_element(self):
+        selected = 1
+        l, _ = self.make_list(1)
+        size = 0
+        l.delete(selected, all=False)
+        self.assertEqual(size, l.len())
+
+    def test_delete_all(self):
+        selected = 1
+        l, _ = self.make_list(1, 1, 1, 42, 1)
+        size = l.len() - 4
+        l.delete(selected, all=True)
+        self.assertEqual(size, l.len())
+
+    def test_clean(self):
+        l, _ = self.make_list(1, 2)
+        l.clean()
+        self.assertEqual(0, l.len())
+
+    def test_len_empty(self):
+        l, _ = self.make_list()
+        self.assertEqual(0, l.len())
+
+    def test_len_non_empty(self):
+        l, _ = self.make_list(1, 2, 42)
+        self.assertEqual(3, l.len())
+
+    def test_insert_start(self):
+        l, _ = self.make_list()
+        self.assertEqual(0, l.len())
+        node = Node(42)
+        l.insert(None, node)
+        self.assertEqual(1, l.len())
+        self.assertEqual(42, l.head.value)
+
+    def test_insert_after(self):
+        l, nodes = self.make_list(2, 2, 3)
+        self.assertEqual(3, l.len())
+        node = Node(42)
+        l.insert(nodes[0], node)
+        self.assertEqual(42, nodes[0].next.value)
