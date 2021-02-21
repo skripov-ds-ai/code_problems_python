782dad5f1725539346c36e27f7b4733fa7d32180
Denis Skripov <nizhikebinesi@gmail.com>
2021-02-22 00:39:07 +0600

    add tree

---
+++ other/algs/tree/tree.py
@@ -0,0 +1,40 @@
+class SimpleTreeNode:
+    def __init__(self, val, parent):
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.Children = []  # список дочерних узлов
+
+
+class SimpleTree:
+    def __init__(self, root):
+        self.Root = root
+        # корень, может быть None
+
+    def AddChild(self, ParentNode, NewChild):
+        # TODO
+        ParentNode.Children.append(NewChild)
+        # pass  # ваш код добавления нового дочернего узла существующему ParentNode
+
+    def DeleteNode(self, NodeToDelete):
+        pass  # ваш код удаления существующего узла NodeToDelete
+
+    def GetAllNodes(self):
+        # ваш код выдачи всех узлов дерева в определённом порядке
+        return []
+
+    def FindNodesByValue(self, val):
+        # ваш код поиска узлов по значению
+        return []
+
+    def MoveNode(self, OriginalNode, NewParent):
+        # ваш код перемещения узла вместе с его поддеревом --
+        # в качестве дочернего для узла NewParent
+        pass
+
+    def Count(self):
+        # количество всех узлов в дереве
+        return 0
+
+    def LeafCount(self):
+        # количество листьев в дереве
+        return 0
