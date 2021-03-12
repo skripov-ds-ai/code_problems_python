1a0650b0bfaa196bc0772d3f4bca75a8fa92e360
Denis Skripov <nizhikebinesi@gmail.com>
2021-03-12 17:54:03 +0600

    draft

---
+++ other/algs/binary_tree/tree.py
@@ -0,0 +1,46 @@
+class BSTNode:
+    def __init__(self, key, val, parent):
+        self.NodeKey = key  # ключ узла
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+
+
+class BSTFind:  # промежуточный результат поиска
+    def __init__(self):
+        self.Node = None  # None если
+        # в дереве вообще нету узлов
+
+        self.NodeHasKey = False  # True если узел найден
+        self.ToLeft = False  # True, если родительскому узлу надо
+        # добавить новый узел левым потомком
+
+
+class BST:
+    def __init__(self, node):
+        self.Root = node  # корень дерева, или None
+
+    def find(self, root, key):
+        if self.Root.
+
+
+    def FindNodeByKey(self, key):
+        # ищем в дереве узел и сопутствующую информацию по ключу
+        return None  # возвращает BSTFind
+
+    def AddKeyValue(self, key, val):
+        # добавляем ключ-значение в дерево
+        return False  # если ключ уже есть
+
+    def FinMinMax(self, FromNode, FindMax):
+        # ищем максимальный/минимальный ключ в поддереве
+        # возвращается объект типа BSTNode
+        return None
+
+    def DeleteNodeByKey(self, key):
+        # удаляем узел по ключу
+        return False  # если узел не найден
+
+    def Count(self):
+        return 0  # количество узлов в дереве
