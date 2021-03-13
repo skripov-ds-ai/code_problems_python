8a51a5eea7a149acf11e961a30b7de12b3b0ece2
Denis Skripov <nizhikebinesi@gmail.com>
2021-03-13 23:10:56 +0600

    bst draft

---
+++ other/algs/binary_tree/test_tree.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+from other.algs.binary_tree.tree import BSTNode, BSTFind, BST
+
+
+class TestTree(TestCase):
+    def make_tree(self):
+        pass
+        # vals = [1, 2, 3, 5, 5]
+        # nodes = []
+        # for val in vals:
+        #     nodes.append(SimpleTreeNode(val, None))
+        # t = SimpleTree(nodes[0])
+        # # 1 -> 2
+        # t.AddChild(nodes[0], nodes[1])
+        # # 1 -> 3
+        # t.AddChild(nodes[0], nodes[2])
+        # # 2 -> 5
+        # t.AddChild(nodes[1], nodes[3])
+        # # 2 -> 5
+        # t.AddChild(nodes[1], nodes[4])
+        # return t, nodes
+
+    def test_Add(self):
+        root = BSTNode(5, 5, None)
+        # print(type(root))
+        t = BST(root)
+        # print(type(t.Root))
+        t.AddKeyValue(4, 4)
+        # print()
+        # print(type(t.Root.LeftChild))
+        # print(type(t.Root.RightChild))
+        # print(type(t.Root))
+        self.assertEqual(t.Root.LeftChild.NodeKey, 4)
+        t.AddKeyValue(3, 3)
+        self.assertEqual(t.Root.LeftChild.LeftChild.NodeKey, 3)
+        t.AddKeyValue(6, 6)
+        self.assertEqual(t.Root.RightChild.NodeKey, 6)
+
+    def test_Find(self):
+        root = BSTNode(5, 5, None)
+        t = BST(root)
+        t.AddKeyValue(4, 4)
+        res = t.FindNodeByKey(5)
+        self.assertTrue(res.NodeHasKey)
+        self.assertEqual(res.Node, root)
+
+        res = t.FindNodeByKey(4)
+        self.assertTrue(res.NodeHasKey)
+        self.assertEqual(res.Node, root.LeftChild)
+
+        res = t.FindNodeByKey(3)
+        self.assertFalse(res.NodeHasKey)
+
+        t.AddKeyValue(7, 7)
+        res = t.FindNodeByKey(7)
+        self.assertTrue(res.NodeHasKey)
+        self.assertEqual(res.Node, root.RightChild)
+
+
+
--- other/algs/binary_tree/tree.py
+++ other/algs/binary_tree/tree.py
@@ -20,27 +20,102 @@ class BSTFind:  # промежуточный результат поиска
 class BST:
     def __init__(self, node):
         self.Root = node  # корень дерева, или None
+        self._len = 1
 
-    def find(self, root, key):
-        if self.Root.
-
+    def find(self, root, key, left):
+        # if root is None:
+        #     t = BSTFind()
+        #     t.Node = root
+        #     t.ToLeft = left
+        #     return t
+        if root.NodeKey == key:
+            t = BSTFind()
+            t.Node = root
+            t.NodeHasKey = True
+            t.ToLeft = left
+            return t
+        if root.NodeKey > key:
+            if root.LeftChild is None:
+                t = BSTFind()
+                t.Node = root
+                t.ToLeft = True
+                return t
+            return self.find(root.LeftChild, key, True)
+        if root.RightChild is None:
+            t = BSTFind()
+            t.Node = root
+            t.ToLeft = False
+            return t
+        return self.find(root.RightChild, key, False)
 
     def FindNodeByKey(self, key):
         # ищем в дереве узел и сопутствующую информацию по ключу
-        return None  # возвращает BSTFind
+        return self.find(self.Root, key, False)
+        # возвращает BSTFind
 
     def AddKeyValue(self, key, val):
         # добавляем ключ-значение в дерево
-        return False  # если ключ уже есть
+        t = self.find(self.Root, key, False)
+        if t.NodeHasKey:
+            return False
+        node = BSTNode(key, val, t.Node)
+        if t.ToLeft:
+            t.Node.LeftChild = node
+        else:
+            t.Node.RightChild = node
+        self._len += 1
+        return True
+        # если ключ уже есть
+
+    def find_min_max(self, root, mx=True):
+        if mx:
+            if root.RightChild:
+                return self.find_min_max(root.RightChild, mx=mx)
+            t = BSTFind()
+            t.Node = root
+            t.NodeHasKey = True
+            return t
+        if root.LeftChild:
+            return self.find_min_max(root.LeftChild, mx=mx)
+        t = BSTFind()
+        t.Node = root
+        t.NodeHasKey = True
+        return t
 
     def FinMinMax(self, FromNode, FindMax):
         # ищем максимальный/минимальный ключ в поддереве
         # возвращается объект типа BSTNode
-        return None
+        return self.find_min_max(FromNode, FindMax)
+
+    def find_successor(self, root):
+        if root.RightChild:
+            node = root.RightChild
+            while node.LeftChild:
+                node = node.LeftChild
+            return node, True
+        return root.LeftChild, False
 
     def DeleteNodeByKey(self, key):
         # удаляем узел по ключу
-        return False  # если узел не найден
+        t = self.find(self.Root, key, False)
+        if not t.NodeHasKey:
+            return False
+        self._len -= 1
+        # TODO: successor find
+        # successor, right = self.find_successor(t.Node)
+        # if successor:
+        #     if not right:
+        #         t.Node = successor
+        #     else:
+        #         t.Node.NodeKey = successor.NodeKey
+        #         t.Node.NodeValue = successor.NodeValue
+        #         # TODO
+        # else:
+        #     t.Node.Parent.
+
+
+        return True  # если узел не найден
 
     def Count(self):
-        return 0  # количество узлов в дереве
+        return self._len
+        # количество узлов в дереве
