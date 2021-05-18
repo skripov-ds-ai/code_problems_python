3f3146688a0ec2a31da26619270dcfeba11e5a71
Denis Skripov <nizhikebinesi@gmail.com>
2021-05-18 22:20:33 +0600

    add graph

---
+++ other/algs/graph/graph.py
@@ -0,0 +1,42 @@
+class Vertex:
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+        # ваш код добавления новой вершины
+        # с значением value
+        # в свободное место массива vertex
+        for i in range(self.max_vertex):
+            if self.vertex[i] is None:
+                self.vertex[i] = Vertex(v)
+                break
+
+    # здесь и далее, параметры v -- индекс вершины
+    # в списке  vertex
+    def RemoveVertex(self, v):
+        # ваш код удаления вершины со всеми её рёбрами
+        self.vertex[v] = None
+        for i in range(self.max_vertex):
+            self.m_adjacency[i][v] = 0
+            self.m_adjacency[v][i] = 0
+
+    def IsEdge(self, v1, v2):
+        # True если есть ребро между вершинами v1 и v2
+        return self.m_adjacency[v1][v2] == 1 and self.m_adjacency[v2][v1] == 1
+
+    def AddEdge(self, v1, v2):
+        # добавление ребра между вершинами v1 и v2
+        self.m_adjacency[v1][v2] = 1
+        self.m_adjacency[v2][v1] = 1
+
+    def RemoveEdge(self, v1, v2):
+        # удаление ребра между вершинами v1 и v2
+        self.m_adjacency[v1][v2] = 0
+        self.m_adjacency[v2][v1] = 0
